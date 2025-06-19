f4cfa4fc0a06e3a85af50c1db5705ec0468b0204
Alejandro <20230542@ipopsa.edu.do>
2025-06-19 17:48:00 -0400

    Actualizacion y optimizacion del frontend

--- frontend/src/pages/user.jsx
+++ frontend/src/pages/user.jsx
@@ -59,7 +59,7 @@ function User() {
                             </a>
                         </li>
                         <li>
-                            <a href="" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <a href="/signOut" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
                                 <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-7 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
                                 </svg>
@@ -86,7 +86,6 @@ function User() {
             </aside>
 
             <div className="p-4 sm:ml-64">
-                <Greeting />
                 <Outlet />
             </div>
         </>
