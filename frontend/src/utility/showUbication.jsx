59d68bc32d1575fc01ff4ef4c1dce71d8bcd1d30
Alejandro <20230542@ipopsa.edu.do>
2025-06-20 21:07:23 -0400

    Actualizacion del frontend

--- frontend/src/utility/showUbication.jsx
+++ frontend/src/utility/showUbication.jsx
@@ -1,15 +1,35 @@
+import { useState, useEffect } from "react";
+
 import UseCurrentLocation from "../hook/useCurrentLocation";
 
 function ShowUbication() {
     const { location, error } = UseCurrentLocation()
+    const [address, setAddress] = useState(null)
+
+    useEffect(() => {
+        if (location) {
+            const { latitude, longitude } = location
+
+            fetch(
+                `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
+            )
+                .then((res) => res.json())
+                .then((data) => {
+                    setAddress(data.display_name);
+                })
+                .catch((err) => {
+                    console.error("Error al obtener la direccion:", err);
+                    setAddress("No se pudo obtener la direccion.");
+                });
+        }
+    }, [location])
 
     if (error) return <p className="text-red-500">{error}</p>
     if (!location) return <p className="text-gray-500">📍 Obteniendo ubicación...</p>
 
     return (
-        <div className="p-4">
-            <p>{location.latitude}</p>
-            <p>{location.longitude}</p>
+        <div className="p-4 text-sm text-gray-800">
+            <p>{address}</p>
         </div>
     )
 }
